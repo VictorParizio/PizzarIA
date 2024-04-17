1c84eec6f6e8665e46c21f6fe630819e5042e4e4
Victor Parizio <victorparizio@hotmail.com>
2024-04-17 16:25:06 -0300

    feat Foi implementado a funcionalidade do contador para adicionar ou reduzir as unidades de pizza

--- src/components/Control/index.jsx
+++ src/components/Control/index.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import "./styles.css";
 
-export const Control = ({variant}) => {
+export const Control = ({ variant }) => {
+  const [counter, setCounter] = useState(0);
+
+  function handleChange(event) {
+    const value = parseInt(event.target.value);
+    setCounter(value);
+  }
+
+  function handleClickLess() {
+    counter <= 0 ? setCounter(0) : setCounter(counter - 1);
+  }
+
+  function handleClickMore() {
+    setCounter(counter + 1);
+  }
+
   return (
     <div className="control">
-      <button className={variant} id="back">-</button>
-      <input className={`${variant}-input`} type="number" value="0" />
-      <button className={variant} id="next">+</button>
+      <button className={variant} id="less" onClick={handleClickLess}>
+        -
+      </button>
+      <input
+        className={`${variant}-input`}
+        type="number"
+        value={counter}
+        onChange={handleChange}
+      />
+      <button className={variant} id="more" onClick={handleClickMore}>
+        +
+      </button>
     </div>
   );
 };
