9ae2668470f43502d09cad86646b846f2cb6f4fe
Victor Parizio <victorparizio@hotmail.com>
2024-04-21 13:17:29 -0300

    feat Foi criada a página 404

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+import img404 from "../../assets/images/404.jpeg";
+import { Button } from "../../components/Button";
+import "./styles.css";
+
 export const NotFound = () => {
-	return(
-		<h2>404 NotFound</h2>
-	)
-}
+  return (
+    <section className="container-404">
+      <img src={img404} alt="página não encontrada" className="img404" />
+      <div>
+        <h2>Ops! Nenhuma fatia por aqui</h2>
+        <p>A página que você está procurando não existe ou foi movida.</p>
+        <p>
+          Enquanto isso, por que não dar uma olhada no nosso delicioso cardápio
+          de pizzas feitas por inteligência artificial? Temos uma variedade de
+          sabores que certamente irão satisfazer seu apetite.
+        </p>
+        <Button variant={"large animation"}>
+          <Link to="/menu">Quero pedir a minha Pizza</Link>
+        </Button>
+      </div>
+    </section>
+  );
+};
