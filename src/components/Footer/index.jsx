bdf07c2e03020a2d4f6741c60935f82850aa584d
Victor Parizio <victorparizio@hotmail.com>
2024-05-13 23:02:22 -0300

    feat: responsividade footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,40 +3,41 @@ import "./styles.css";
 
 export const Footer = () => {
   return (
-    <footer>
-      <ul className="links-utils">
-        <li>
-          <a
-            href="https://www.linkedin.com/in/victorpariziobackend/"
-            target="_blank"
-          >
-            <FaLinkedin />
-          </a>
-        </li>
+    <div className="bg-container">
+      <footer>
+        <ul className="links-utils">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/victorpariziobackend/"
+              target="_blank"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
 
-        <li>
-          <a href="https://github.com/VictorParizio" target="_blank">
-            <FaGithub />
-          </a>
-        </li>
+          <li>
+            <a href="https://github.com/VictorParizio" target="_blank">
+              <FaGithub />
+            </a>
+          </li>
 
-        <li>
-          <a
-            href="https://portfolio-victor-parizio.vercel.app/"
-            target="_blank"
-          >
-            <FaUserTie />
-          </a>
-        </li>
-      </ul>
+          <li>
+            <a
+              href="https://portfolio-victor-parizio.vercel.app/"
+              target="_blank"
+            >
+              <FaUserTie />
+            </a>
+          </li>
+        </ul>
 
-      <p>
-        Desenvolvido por <strong>Victor Parizio</strong>
-      </p>
+        <p>
+          Desenvolvido por <strong>Victor Parizio</strong>
+        </p>
 
-      <div className="top">
-        <button className="topo"></button>
-      </div>
-    </footer>
+        <div className="top">
+        </div>
+      </footer>
+    </div>
   );
 };
