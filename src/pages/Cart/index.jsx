33e85c26e0d34b13600e2a668912aeab434f94ba
Victor Parizio <victorparizio@hotmail.com>
2024-05-13 23:03:14 -0300

    feat: responsividade Cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 import { CartContext } from "../../context/cartContext";
 import { useCartContext } from "../../hooks/useCartContext";
@@ -14,6 +14,19 @@ export const Cart = () => {
   const { cart, setCart } = useContext(CartContext);
   const { removeProductCart, totalCart } = useCartContext();
   const [removingItemId, setRemovingItemId] = useState(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 568);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 568);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleRemoveProduct = (id) => {
     setRemovingItemId(id);
@@ -34,39 +47,78 @@ export const Cart = () => {
 
   return (
     <section className="container-shopping">
-      <section className="my-shopping-cart">
-        <div className="th-product-card">
-          <p>Produto</p>
-          <p>Preço</p>
-          <p>Quantidade</p>
-          <p>Subtotal</p>
-        </div>
-        <ul>
-          {cart.map((item) => (
-            <li
-              className={`product-card ${
-                removingItemId === item.product_id ? "remove-item" : ""
-              }`}
-              key={item.product_id}
-            >
-              <div className="product">
+      {!isMobile && (
+        <section className="my-shopping-cart">
+          <div className="th-product-card">
+            <p>Produto</p>
+            <p>Preço</p>
+            <p>Quantidade</p>
+            <p>Subtotal</p>
+          </div>
+          <ul>
+            {cart.map((item) => (
+              <li
+                className={`product-card ${
+                  removingItemId === item.product_id ? "remove-item" : ""
+                }`}
+                key={item.product_id}
+              >
+                <div className="product">
+                  <img src={item.product_image_url} alt={item.product_name} />
+                  <p>{item.product_name}</p>
+                </div>
+                <p>{formatCurrency(item.product_price)}</p>
+                <Control
+                  variant={"small"}
+                  cartItem={item}
+                  remove={handleRemoveProduct}
+                />
+                <p>{formatCurrency(item.product_price * item.quantity)}</p>
+                <FaTrashAlt
+                  onClick={() => handleRemoveProduct(item.product_id)}
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      {isMobile && (
+        <section className="my-shopping-cart">
+          <ul>
+            {cart.map((item) => (
+              <li
+                className={`product-card ${
+                  removingItemId === item.product_id ? "remove-item" : ""
+                }`}
+                key={item.product_id}
+              >
                 <img src={item.product_image_url} alt={item.product_name} />
-                <p>{item.product_name}</p>
-              </div>
-              <p>{formatCurrency(item.product_price)}</p>
-              <Control
-                variant={"small"}
-                cartItem={item}
-                remove={handleRemoveProduct}
-              />
-              <p>{formatCurrency(item.product_price * item.quantity)}</p>
-              <FaTrashAlt
-                onClick={() => handleRemoveProduct(item.product_id)}
-              />
-            </li>
-          ))}
-        </ul>
-      </section>
+
+                <div>
+                  <p className="product-name">{item.product_name}</p>
+                  <Control
+                    variant={"small"}
+                    cartItem={item}
+                    remove={handleRemoveProduct}
+                  />
+                </div>
+
+                <div className="price">
+                  <p className="label">Preço</p>
+                  <p>{formatCurrency(item.product_price)}</p>
+                  <p className="label">Subtotal</p>
+                  <p>{formatCurrency(item.product_price * item.quantity)}</p>
+                </div>
+                
+                <FaTrashAlt
+                  onClick={() => handleRemoveProduct(item.product_id)}
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
 
       <section className="cart-total">
         <h3>Total do Carrinho</h3>
