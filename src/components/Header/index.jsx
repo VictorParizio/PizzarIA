f058955e9172205133a6382ed0f347408649d5a7
Victor Parizio <victorparizio@hotmail.com>
2024-05-15 12:55:36 -0300

    fix: alinhamento do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { UserAuthContext } from "../../context/userAuthContext";
 
@@ -19,7 +19,19 @@ export const Header = () => {
   const { totalItems } = useCartContext();
   const { usuarioLogado, setUsuarioLogado } = useContext(UserAuthContext);
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
   const [showModalCart, setShowModalCart] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [isClose, setIsClose] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleLogout = () => {
     sessionStorage.removeItem("token");
@@ -30,53 +42,111 @@ export const Header = () => {
     setShowModalCart(!showModalCart);
   };
 
+  const ToggleMode = () => {
+    setActiveMenu(!activeMenu);
+    setTimeout(() => {
+      setIsClose(!isClose);
+    }, 1000);
+  };
+
   return (
-    <header className="navigation">
-      <Link to="/" className="logo">
-        PizzarIA
-      </Link>
+    <>
+      {" "}
+      <header className="navigation">
+        <Link to="/" className="logo">
+          PizzarIA
+        </Link>
 
-      {usuarioLogado && (
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/menu">Cardápio</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cart">Carrinho</NavLink>
-            </li>
-          </ul>
-        </nav>
-      )}
-
-      <div className="member-area">
-        {!usuarioLogado && location.pathname !== "/login" && (
-          <Link to="/login">Entrar</Link>
-        )}
-
-        {!usuarioLogado && location.pathname === "/login" && (
-          <Link to="/signup">Cadastrar</Link>
-        )}
-
-        {usuarioLogado && (
+        {!isMobile && (
           <>
-            <Link to="#" onClick={handleModalToggle}>
-              <FaCartShopping title="Carrinho suspenso" />
-              <strong title="Total de itens no carrinho">
-                {totalItems(cart)}
-              </strong>
-            </Link>
-            <Link to="/404">
-              <FaUser title="Perfil do usuário" />
-            </Link>
-            <Link to="/">
-              <FiLogOut className="close" title="Sair" onClick={handleLogout} />
-            </Link>
+            {usuarioLogado && (
+              <nav>
+                <ul>
+                  <li>
+                    <NavLink to="/menu">Cardápio</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cart">Carrinho</NavLink>
+                  </li>
+                </ul>
+              </nav>
+            )}
+
+            <div className="member-area">
+              {!usuarioLogado && location.pathname !== "/login" && (
+                <Link to="/login">Entrar</Link>
+              )}
+
+              {!usuarioLogado && location.pathname === "/login" && (
+                <Link to="/signup">Cadastrar</Link>
+              )}
+
+              {usuarioLogado && (
+                <>
+                  <Link to="/404">
+                    <FaUser title="Perfil do usuário" />
+                  </Link>
+                  <Link to="/">
+                    <FiLogOut
+                      className="close"
+                      title="Sair"
+                      onClick={handleLogout}
+                    />
+                  </Link>
+                </>
+              )}
+            </div>
+
+            {showModalCart && <ModalCart isOpen={handleModalToggle} />}
           </>
         )}
-      </div>
 
-      {showModalCart && <ModalCart  isOpen={handleModalToggle} />}
-    </header>
+        {isMobile && usuarioLogado && (
+          <>
+            <div
+              className={activeMenu ? "icon iconActive" : "icon"}
+              onClick={ToggleMode}
+            >
+              <div className="hamburger hamburgerIcon" />
+            </div>
+
+            <div
+              className={
+                activeMenu ? "menu menuOpen" : `menu menuClose ${!isClose}`
+              }
+            >
+              <nav className="list">
+                <ul className="listItems">
+                  <li>
+                    <NavLink to="/menu" onClick={ToggleMode}>
+                      Cardápio
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/cart" onClick={ToggleMode}>
+                      Carrinho
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/404" onClick={ToggleMode}>
+                      Perfil
+                    </NavLink>
+                  </li>
+                  <li>
+                    <Link to="/">Sair</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </>
+        )}
+      </header>
+      {usuarioLogado && (
+        <Link to="#" className="cart" onClick={handleModalToggle}>
+          <FaCartShopping title="Carrinho suspenso" />
+          <strong title="Total de itens no carrinho">{totalItems(cart)}</strong>
+        </Link>
+      )}
+    </>
   );
 };
